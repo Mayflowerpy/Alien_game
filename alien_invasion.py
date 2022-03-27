9a135a46c63f8ff5fe03d6bca158934cbf234aa6
Vladislav Shilov <95354582+Mayflowerpy@users.noreply.github.com>
2022-03-27 23:25:19 +0300

    Upload #5

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
@@ -16,7 +18,7 @@ class AlienInvasion:
         pygame.init()     #Инициализирует настройки игры
         self.settings = Settings()
         
-        # Полноэкранный режим:
+        # Полноэкранный режим:q
         self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
@@ -28,6 +30,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion") # Название окна
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -35,6 +41,7 @@ class AlienInvasion:
             #Отслеживание событий клавиатуры и мыши
             self._check_events()
             self.ship.update() # Обновление позиции корабля
+            self._update_bullets()
             #При каждом проходе цикла перерисовывается экран и корабль
             self._update_screen()
 
@@ -54,6 +61,8 @@ class AlienInvasion:
                 self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
                 self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -62,13 +71,39 @@ class AlienInvasion:
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
-            
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Создание нового снаряда и включение его в группу bullets"""
+        if len(self.bullets) < self.settings.bullets_allowed: # Првоерка, что на экране 3 снаряда
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды"""
+        # Обновление позиции снаряда
+        self.bullets.update() # Обновление прорисовки пуль
+
+        # Удаление снарядов, вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.top <= 0:
+                self.bullets.remove(bullet)
+            #print(len(self.bullets)) - Проверка удаления
+
+    def _create_fleet(self):
+        """Создание флота пришельцев"""
+        # Создание пришельца
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme() #Корабль рисуется вызовом этой команды
+        # Чтобы пули были нарисованы:
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         #Отображение последнего прорисованного экрана
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -16,5 +16,5 @@ class Settings():
         self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        
+        self.bullet_color = (255, 255, 255) # Цвет
+        self.bullets_allowed = 3 # Максимальное кол-ва пуль на экране
